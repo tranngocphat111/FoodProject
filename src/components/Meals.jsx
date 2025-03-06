5079b231700a4109f01113802c05a9039966292e
Trần Ngọc Phát <tranngocphat240111@gmail.com>
2025-03-06 13:15:22 +0700

    fix

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -7,8 +7,11 @@ export default function Meals() {
         <div className="w-[80%] p-4 mx-auto">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
             {data.map((meal, index) => 
-              {index < 6 &&   <Meal key={meal.id} meal={meal} />}
-            
+              {
+                if(index < 6){
+                  return <Meal key={meal.id} meal={meal} />
+                }
+              }
             )}
           </div>
         </div>
