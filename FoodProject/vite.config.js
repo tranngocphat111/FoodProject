68f3ba94a8465d03b493abbd6c83fff9d5a571bc
Trần Ngọc Phát <tranngocphat240111@gmail.com>
2025-03-08 09:42:17 +0700

    fix

--- FoodProject/vite.config.js
+++ FoodProject/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/FoodProject/",  // ⚠️ Đảm bảo có dòng này
+  // base: "/FoodProject/",  // ⚠️ Đảm bảo có dòng này
   plugins: [react()],
 });
